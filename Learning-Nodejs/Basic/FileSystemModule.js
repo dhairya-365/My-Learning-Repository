1e300bf8f29ae646c2f9f4ec373fb01450d1e97a
Dhairya1910 <dhairyavaghela12@gmail.com>
2026-07-15 09:55:08 +0530

    Learned and Implemented Async File system operation using fs module.

---
+++ Learning-Nodejs/Basic/FileSystemModule.js
@@ -0,0 +1,29 @@
+const { readFile, writeFile } = require('fs');
+
+readFile("Learning-Nodejs/Docs/first.txt", "utf-8", (err, results) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    const first_content = results;
+
+    // once we are done reading first.txt its time to read second.txt 
+
+    readFile("Learning-Nodejs/Docs/second.txt", "utf-8", (err, results) => {
+        if (err) {
+            console.log(err);
+            return
+        }
+        const sec_content = results;
+
+        // once we are reading both files it time to store the results in our main files.
+
+        writeFile("Learning-Nodejs/Docs/test.txt", `The results : ${first_content}, ${sec_content}`, (err, results) => {
+            if (err) {
+                console.log(err);
+                return
+            }
+            console.log(results);
+        })
+    })
+})
