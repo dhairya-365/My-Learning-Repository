fee021bc893e2586298ca6be1f95c6dcd043f3b5
Dhairya1910 <dhairyavaghela12@gmail.com>
2026-07-15 18:03:11 +0530

    Installed and configured Express.js:

--- Learning-Express/Express-Tutorials/practice1.js
+++ Learning-Express/Express-Tutorials/practice1.js
@@ -1,11 +1,23 @@
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((req, res) => {
-    console.log("User hits the server.");
-    console.log(req.url);
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>Welcome to my server.</>");
-    res.end();
-});
+// const server = http.createServer((req, res) => {
+//     console.log("User hits the server.");
+//     console.log(req.url);
+//     res.writeHead(200, { "content-type": "text/html" });
+//     res.write("<h1>Welcome to my server.</>");
+//     res.end();
+// });
 
-server.listen(5050);
+// server.listen(5050);
+
+const express = require('express');
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
