57f02247b2f321df2f9a675062b4da9beec8cbf8
Dhairya1910 <dhairyavaghela12@gmail.com>
2026-07-16 13:05:19 +0530

    Added more entries.

--- Learning-Express/Express-Tutorials/data/employee.js
+++ Learning-Express/Express-Tutorials/data/employee.js
@@ -1,7 +1,7 @@
 const info = [
     {
         "id": 45,
-        "Name": "Dhairya Vaghela",
+        "Name": "Dhairya",
         "Age": 22,
         "Designation": "AI engineer"
     },
@@ -10,6 +10,96 @@ const info = [
         "Name": "Virat",
         "Age": 39,
         "Designation": "Batsman"
+    },
+    {
+        "id": 7,
+        "Name": "MS Dhoni",
+        "Age": 42,
+        "Designation": "Wicketkeeper"
+    },
+    {
+        "id": 93,
+        "Name": "Jasprit",
+        "Age": 30,
+        "Designation": "Fast Bowler"
+    },
+    {
+        "id": 8,
+        "Name": "Ravindra",
+        "Age": 35,
+        "Designation": "All-rounder"
+    },
+    {
+        "id": 33,
+        "Name": "Hardik",
+        "Age": 30,
+        "Designation": "All-rounder"
+    },
+    {
+        "id": 77,
+        "Name": "Shubman",
+        "Age": 24,
+        "Designation": "Opening Batsman"
+    },
+    {
+        "id": 1,
+        "Name": "KL Rahul",
+        "Age": 32,
+        "Designation": "Wicketkeeper Batsman"
+    },
+    {
+        "id": 99,
+        "Name": "Ashwin",
+        "Age": 37,
+        "Designation": "Spin Bowler"
+    },
+    {
+        "id": 10,
+        "Name": "Sachin",
+        "Age": 51,
+        "Designation": "Legendary Batsman"
+    },
+    {
+        "id": 22,
+        "Name": "Kane",
+        "Age": 33,
+        "Designation": "Batsman"
+    },
+    {
+        "id": 49,
+        "Name": "Steve",
+        "Age": 35,
+        "Designation": "Batsman"
+    },
+    {
+        "id": 56,
+        "Name": "Babar",
+        "Age": 29,
+        "Designation": "Batsman"
+    },
+    {
+        "id": 19,
+        "Name": "Rashid",
+        "Age": 25,
+        "Designation": "Spin Bowler"
+    },
+    {
+        "id": 21,
+        "Name": "Pat",
+        "Age": 31,
+        "Designation": "Fast Bowler"
+    },
+    {
+        "id": 101,
+        "Name": "Aditi",
+        "Age": 26,
+        "Designation": "Data Scientist"
+    },
+    {
+        "id": 102,
+        "Name": "Rohan",
+        "Age": 28,
+        "Designation": "Frontend Developer"
     }
 ]
 
