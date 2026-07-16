e1b4171d9487bcef0bb831ddece978dbf6b79f8c
Dhairya1910 <dhairyavaghela12@gmail.com>
2026-07-16 11:51:45 +0530

    Created a data folder to store data in json format.

---
+++ Learning-Express/Express-Tutorials/data/employee.js
@@ -0,0 +1,16 @@
+const info = [
+    {
+        "id": 45,
+        "Name": "Dhairya Vaghela",
+        "Age": 22,
+        "Designation": "AI engineer"
+    },
+    {
+        "id": 18,
+        "Name": "Virat",
+        "Age": 39,
+        "Designation": "Batsman"
+    }
+]
+
+module.exports = { info };
